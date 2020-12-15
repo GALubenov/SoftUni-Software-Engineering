61fdee546782476fdeb4925d431ad242430b5903
GALubenov <joro.75@abv.bg>
2020-12-15 16:13:01 +0200

    JA basics-Lab-Exercise-Exams

--- JS-Exam-6-7.04.2019/01.oskar.js
+++
@@ -1,8 +0,0 @@
-function oskar(args){
-let naem = Number(args[0]);
-let statue = naem - (naem * 0.30);
-let ketering = statue - (statue *0.15);
-let sound = ketering - (ketering /2);
-console.log((naem + statue + ketering + sound).toFixed(2))
-}
-oskar([3500])
