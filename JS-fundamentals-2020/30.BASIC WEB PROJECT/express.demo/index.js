d18111f029934bd5a83b509572a81900eb65a84c
GALubenov <joro.75@abv.bg>
2020-12-22 12:53:10 +0200

    22.12.20

--- JS-fundamentals-2020/30.BASIC WEB PROJECT/express.demo/index.js
+++ JS-fundamentals-2020/30.BASIC WEB PROJECT/express.demo/index.js
@@ -1,36 +1,38 @@
+// npm init
+// npm install express --save
+// npm install express-handlebars --save
+
 let express = require('express'); // ctrl + c -> stop server
+let handlebars = require('express-handlebars');
+let fs = require('fs');
 
 let app = express();
 let port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.engine('hbs', handlebars());
+app.set('view engine', 'hbs');
 
+//app.get('/', function (req, res) { res.send('Hello World!') });
+app.get('/', (req, res) => { res.render('home', { layout: false }) });
+
+//app.get('/catalog', (req, res) => { res.send('Catalog page!') });
 app.get('/catalog', (req, res) => {
-    res.send('Catalog page!');
+    let products = JSON.parse(fs.readFileSync('./data/products.json', 'utf-8')); // това е защото продуктите са хардкорднати и 
+    //искаме да ги зареждаме динамично
+    res.render('catalog', { layout: false, products });
 });
 
 app.get('/catalog/:serial_numbers', (req, res) => {
-    let products = [
-        '123',
-        '456',
-        '789'
-    ]
-
+    //  let products = ['123', '456', '789']
+    let products = JSON.parse(fs.readFileSync('./data/products.json', 'utf-8'));
     let sn = req.params.serial_numbers;
-
     if (products.find(x => x === sn)) {
-        res.send(`
-    <h1>Catalog page!</h1>
-    <p>Product Number: ${sn}</p>
-    `);
+        res.render('details', { layout: false, sn });
     } else {
         res.status(404);
-        res.send(`<p>Error 404: Product with S/N ${sn} was not found!</p>`);
+        res.render('404', { layout: false });
     }
 });
-
 
 app.listen(port, () => {
     console.log(`Port listening on port ${port}`);
